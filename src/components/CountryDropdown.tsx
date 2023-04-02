b9f81b73b9a7f31c33a7fcbc73c0e2e86adce896
ilya <zapaska96@gmail.com>
2023-04-02 16:41:38 +0500

    cd

---
+++ src/components/CountryDropdown.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { Menu } from '@headlessui/react';
+import { RiArrowDownSLine, RiArrowUpSLine, RiMapPinLine } from 'react-icons/ri';
+import { useAppDispatch, useAppSelector } from '../hooks/redux';
+import { houseSlice } from '../store/houseSlice';
+
+const CountryDropdown = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const { countries, country } = useAppSelector((state) => state.house);
+    const { changeCountry } = houseSlice.actions;
+    const dispatch = useAppDispatch();
+
+    const handleBtnClick = () => {
+        setIsOpen((prev) => !prev);
+    };
+
+    const handleCountryClick = (c: string) => {
+        dispatch(changeCountry(c));
+    };
+
+    return (
+        <Menu as='div' className='dropdown relative'>
+            <Menu.Button
+                className='dropdown-btn w-full text-left'
+                onClick={handleBtnClick}
+            >
+                <RiMapPinLine className='dropdown-icon-primary' />
+                <div>
+                    <div className='text-[15px] font-medium leading-tight'>
+                        {country}
+                    </div>
+                    <div className=''>Select your place</div>
+                </div>
+                {isOpen ? (
+                    <RiArrowDownSLine className='dropdown-icon-secondary' />
+                ) : (
+                    <RiArrowUpSLine className='dropdown-icon-secondary' />
+                )}
+            </Menu.Button>
+            <Menu.Items className='dropdown-menu'>
+                {countries.map((c, idx) => (
+                    <Menu.Item
+                        className='cursor-pointer transition hover:text-violet-700'
+                        as='li'
+                        key={idx}
+                        onClick={() => handleCountryClick(c)}
+                    >
+                        {c}
+                    </Menu.Item>
+                ))}
+            </Menu.Items>
+        </Menu>
+    );
+};
+
+export default CountryDropdown;
