c90fea576c8220f72a0534f33fdc8309703276f5
ilya <zapaska96@gmail.com>
2023-04-03 17:40:26 +0500

    pagination

--- src/components/HouseList.tsx
+++ src/components/HouseList.tsx
@@ -2,11 +2,35 @@ import React from 'react';
 import { useAppSelector } from '../hooks/redux';
 import House from './House';
 import { Link } from 'react-router-dom';
+import usePagination from '../hooks/usePagination';
 
 const HouseList = () => {
     const { filteredHouses } = useAppSelector((state) => state.house);
 
-    if (filteredHouses.length < 1) {
+    const paginationProps = {
+        itemsPerPage: 6,
+        totalItems: filteredHouses.length,
+    };
+
+    const { currentItems, setCurrentPage, currentPage } = usePagination(
+        filteredHouses,
+        paginationProps
+    );
+
+    const totalPagesNumber = Math.ceil(
+        filteredHouses.length / paginationProps.itemsPerPage
+    );
+
+    const totalPages = Array.from(
+        { length: totalPagesNumber },
+        (_, i) => i + 1
+    );
+
+    const handlePageChange = (pageNumber: number) => {
+        setCurrentPage(pageNumber);
+    };
+
+    if (currentItems.length < 1) {
         return (
             <div className='mt-48 text-center text-3xl text-gray-400'>
                 Sorry, nothing found
@@ -17,12 +41,29 @@ const HouseList = () => {
     return (
         <section className='mb-20'>
             <div className='container mx-auto'>
-                <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3 lg:gap-14'>
-                    {filteredHouses.map((item) => (
+                <div className='mb-8 grid gap-6 md:grid-cols-2 lg:grid-cols-3 lg:gap-14'>
+                    {currentItems.map((item) => (
                         <Link to={`/property/${item.id}`} key={item.id}>
                             <House houseObj={item} />
                         </Link>
                     ))}
+                </div>
+                <div className='flex items-center justify-center gap-4'>
+                    {totalPages.length > 1 &&
+                        totalPages.map((page) => (
+                            <button
+                                key={page}
+                                onClick={() => handlePageChange(page)}
+                                disabled={page === currentPage}
+                                className={`border px-5 py-3 text-lg text-violet-500 ${
+                                    page === currentPage
+                                        ? 'border-violet-500'
+                                        : ''
+                                }`}
+                            >
+                                {page}
+                            </button>
+                        ))}
                 </div>
             </div>
         </section>
---
+++ src/hooks/usePagination.ts
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { housesType } from '../data';
+
+interface PaginationProps {
+    itemsPerPage: number;
+    totalItems: number;
+}
+
+interface UsePaginationReturn {
+    currentPage: number;
+    pages: number[];
+    setCurrentPage: (pageNumber: number) => void;
+    currentItems: housesType[];
+}
+
+const usePagination = (
+    data: housesType[],
+    paginationProps: PaginationProps
+): UsePaginationReturn => {
+    const { itemsPerPage, totalItems } = paginationProps;
+    const [currentPage, setCurrentPage] = useState(1);
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+    const getPaginatedData = (): housesType[] => {
+        const startIndex = (currentPage - 1) * itemsPerPage;
+        const endIndex = startIndex + itemsPerPage;
+        return data.slice(startIndex, endIndex);
+    };
+
+    const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+
+    const currentItems = getPaginatedData();
+
+    console.log('page');
+
+    return {
+        currentPage,
+        pages,
+        setCurrentPage,
+        currentItems,
+    };
+};
+
+export default usePagination;
